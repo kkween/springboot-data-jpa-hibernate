6d50b2408593e7d4218a2716886bbc3b1370772f
kkween <104529607+kkween@users.noreply.github.com>
2022-10-16 10:51:24 -0400

    Delete springdatajpa/springboot directory

--- springdatajpa/springboot/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springdatajpa.springboot.repository;
-
-import com.springdatajpa.springboot.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}

b1ad71752ddea86b9ce9e700f79c327dae4cf99e
kkween <104529607+kkween@users.noreply.github.com>
2022-10-16 10:46:27 -0400

    Delete spring-boot-jpa-hibernate/spring-boot-jpa-course-main directory

--- spring-boot-jpa-hibernate/spring-boot-jpa-course-main/src/main/java/com/springdatajpa/springboot/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springdatajpa.springboot.repository;
-
-import com.springdatajpa.springboot.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    Order findByOrderTrackingNumber(String orderTrackingNumber);
-}
